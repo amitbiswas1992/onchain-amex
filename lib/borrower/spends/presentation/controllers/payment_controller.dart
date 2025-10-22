a6b6b2053c4d6b521a5f8153e721335b1bea9ba6
Safius Sifat <safiussifat@gmail.com>
2025-10-22 22:15:19 +0600

    fix: allowance not needed

--- lib/borrower/spends/presentation/controllers/payment_controller.dart
+++ lib/borrower/spends/presentation/controllers/payment_controller.dart
@@ -46,14 +46,14 @@ class PaymentController {
     }
 
     try {
-      final allowance = await walletService.getUsdcAllowance();
-      print('Current allowance: $allowance, required: $amount');
-      if (allowance < amount) {
-        await walletService.approveUsdc(amount);
-        showLoadingDialog(context: context, message: 'Waiting for approval...');
-        await Future.delayed(const Duration(seconds: 2));
-        hideDialog();
-      }
+      // final allowance = await walletService.getUsdcAllowance();
+      // print('Current allowance: $allowance, required: $amount');
+      // if (allowance < amount) {
+      //   await walletService.approveUsdc(amount);
+      //   showLoadingDialog(context: context, message: 'Waiting for approval...');
+      //   await Future.delayed(const Duration(seconds: 2));
+      //   hideDialog();
+      // }
       final txHash = await walletService.spend(
         amount: amount,
         merchantPublicAddress: scannedData.walletAddress!,
